97f20c0dfcc525a89870eacd2476cd454bb87021
Jeremy Boyd <jeremyboyd@pm.me>
2022-04-15 09:56:44 -0600

    Update to use polished_config(). Previous function global_sessins_config() is deprecated.

--- global.R
+++ global.R
@@ -5,9 +5,10 @@ library(polished)
 library(tidyverse)
 library(readxl)
 library(leaflet)
+library(googlesheets4)
 
 # Configure the global sessions when the app initially starts up.
-polished::global_sessions_config(
+polished::polished_config(
     app_name = "giraffe_map_shiny",
     api_key = "v1GDJ310gbQGvLFOboOmpD3HPAP4FHtZS0"
 )
